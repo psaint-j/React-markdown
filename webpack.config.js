0943b5418277a7fd376ab5c162c5456c8fd4a10a
psaint-j <psaint-j@student.42.fr>
2017-05-19 07:09:32 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+
+module.exports = {
+context: __dirname,
+		 entry: './src/app.js',
+		 output: {
+path: __dirname + '/bin',
+	  filename: 'app.output.js',
+		 },
+resolve: {
+extensions: ['', '.js', '.jsx'],
+		 },
+module: {
+loaders: [
+		 {
+test: /\.jsx$|\.js$/,
+	   exclude: /node_modules/,
+	   loader: 'babel-loader',
+		 },
+		 {
+test: /\.css$/,
+	   loaders: ['style', 'css-loader'],
+		 },
+],
+		},
+plugins: [
+			 new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+],
+}
